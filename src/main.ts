84c7f3ad71f92b9c531a0b44e8789caec8217ad2
Tomasz Fraczek <t.fraczek@gmi.pl>
2025-06-27 17:19:17 +0200

    feat: enable CORS for Next.js integration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // adres Next.js
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // true jeśli wysyłasz cookies / bearer
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
